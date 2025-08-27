8996e25fe77423cf9e8e2ee81b4fa2295fb61b08
mengxl <youdou173339@163.com>
2025-08-27 15:18:06 +0800

    feature: 添加json-server, axios 并测试

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,10 @@
+import { get } from "@/utils/request";
 import { FlashList } from "@shopify/flash-list";
 import { defaultConfig } from "@tamagui/config/v4";
 import { TamaguiProvider, createTamagui } from "@tamagui/core";
 import { Image } from "expo-image";
-import { View } from "react-native";
+import { useEffect, useState } from "react";
+import { RefreshControl, View } from "react-native";
 import { Text } from "tamagui";
 
 // Tamagui 配置
@@ -14,53 +16,41 @@ declare module "@tamagui/core" {
   interface TamaguiCustomConfig extends Conf {}
 }
 
-// 扁平化数据
-const DATA = [
-  {
-    id: 1,
-    title: "打了300年的史莱姆，不知不觉就练到了满级",
-    image:
-      "https://i2.hdslb.com/bfs/bangumi/image/d9727717ee6b507225b26ae367a43a444a6157ca.png@308w_410h_1c.avif",
-  },
-  {
-    id: 2,
-    title: "Ｄｒ．ＳＴＯＮＥ 石纪元",
-    image:
-      "https://i2.hdslb.com/bfs/bangumi/image/834bdb5d36c401179b9e91fdc1de520a6664c2a7.png@308w_410h_1c.avif",
-  },
-  {
-    id: 3,
-    title: "齐木楠雄的灾难（日播&精选版）",
-    image:
-      "https://i2.hdslb.com/bfs/bangumi/eb4f17335f48951945fb9da47e6ee0bc65fa2fbb.jpg@308w_410h_1c.avif",
-  },
-  {
-    id: 4,
-    title: "打了300年的史莱姆，不知不觉就练到了满级",
-    image:
-      "https://i2.hdslb.com/bfs/bangumi/image/d9727717ee6b507225b26ae367a43a444a6157ca.png@308w_410h_1c.avif",
-  },
-  {
-    id: 5,
-    title: "Ｄｒ．ＳＴＯＮＥ 石纪元",
-    image:
-      "https://i2.hdslb.com/bfs/bangumi/image/834bdb5d36c401179b9e91fdc1de520a6664c2a7.png@308w_410h_1c.avif",
-  },
-  {
-    id: 6,
-    title: "齐木楠雄的灾难（日播&精选版）",
-    image:
-      "https://i2.hdslb.com/bfs/bangumi/eb4f17335f48951945fb9da47e6ee0bc65fa2fbb.jpg@308w_410h_1c.avif",
-  },
-  // 如果有更多数据，继续添加
-];
+interface Video {
+  id: number;
+  title: string;
+  image: string;
+}
 
 export default function App() {
+  const [videos, setVideos] = useState<Video[]>([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const getVideos = async () => {
+    const res = await get<Video[]>("/videos");
+    return res;
+  };
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    // 模拟异步数据加载
+    getVideos().then((res) => {
+      setVideos(res);
+      setRefreshing(false);
+    });
+  };
+
+  useEffect(() => {
+    getVideos().then((res) => {
+      setVideos(res);
+    });
+  }, []);
+
   return (
     <TamaguiProvider config={config}>
       <View style={{ flex: 1, padding: 2 }}>
         <FlashList
-          data={DATA}
+          data={videos}
           numColumns={3} // 每行显示 3 个元素
           estimatedItemSize={150}
           renderItem={({ item }) => (
@@ -82,6 +72,14 @@ export default function App() {
             </View>
           )}
           keyExtractor={(item) => item.id + ""}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+              tintColor="#000" // iOS 刷新指示器颜色
+              colors={["#000"]} // Android 刷新指示器颜色
+            />
+          }
         />
       </View>
     </TamaguiProvider>
---
+++ utils/request.ts
@@ -0,0 +1,49 @@
+// request.js
+import axios from "axios";
+
+// 创建 axios 实例
+const request = axios.create({
+  baseURL: "http://192.168.18.36:3000", // 你的后端地址
+  timeout: 10000, // 超时时间 10s
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// 请求拦截器
+request.interceptors.request.use(
+  async (config) => {
+    // 在请求前统一处理，比如添加 token
+    const token = "your_token"; // 可以从 Redux / AsyncStorage 拿
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// 响应拦截器
+request.interceptors.response.use(
+  (response) => {
+    // 统一处理响应
+    return response.data; // 直接返回 data 部分
+  },
+  (error) => {
+    // 统一错误处理
+    if (error.response) {
+      console.log("接口错误：", error.response.status, error.response.data);
+    } else if (error.request) {
+      console.log("请求未响应：", error.request);
+    } else {
+      console.log("请求设置错误：", error.message);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default request;
+
+export function get<T>(url: string) {
+  return request.get<any, T>(url);
+}
